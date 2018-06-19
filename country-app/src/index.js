11f86c665160a6a12ecc57939151d2f27279da85
Szymon Grabowski <szmiko@wp.pl>
2018-06-19 20:52:44 +0200

    Files

---
+++ country-app/src/index.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { render } from 'react-dom';
+import { Provider } from 'react-redux';
+import store from './store/index.js';
+import DevTools from './DevTools.js';
+import { getCountries } from './actions/actions-countries.js';
+
+ReactDOM.render(
+	<Provider store={store}>
+	<div>
+		<h1>Inicjalizacja projektu</h1>
+		<DevTools />
+	</div>
+	</Provider>
+	document.getElementById('root')
+);
+
+store.dispatch(getCountries());
